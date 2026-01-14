15aa4416233b79c08fbb18404fb310f5c656e9a9
ulises farias <ulises.farias@encodelabs.com.ar>
2026-01-14 12:19:38 -0300

    ..

--- audit-service/src/Audit.API/Program.cs
+++ audit-service/src/Audit.API/Program.cs
@@ -4,22 +4,34 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+//controllers
 builder.Services.AddControllers();
 
 // Swagger
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();   
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.EnvironmentName == "local")
 {
     //app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+//logs del kestrel que expone el puerto 
+app.Lifetime.ApplicationStarted.Register(() =>
+{
+    var addresses = app.Urls;
+    foreach (var address in addresses)
+    {
+        app.Logger.LogInformation($"Aplicación escuchando en: {address}");
+    }
+});
+
 app.UseHttpsRedirection();
+app.UseAuthorization();
 app.MapControllers();
 app.Run();
