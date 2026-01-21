c888986abe90954588a2889fa0c89ddee51912fd
ulises farias <fariasulisesnicolas@gmail.com>
2026-01-21 15:48:50 -0300

    hola

--- email-service/src/main.ts
+++ email-service/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe,Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,23 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(9000);
+  const port = process.env.PORT ?? 9000;
+  
+  //Swagger Config
+  const config = new DocumentBuilder()
+    .setTitle('UserServices.API')
+    .setBasePath(`http://localhost:${port}/api`)
+    .setDescription('Documentación de la API con Swagger del microservicio de user')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT ?? 9000);
+
+  const logger = new Logger('Bootstrap');
+  logger.log(`La aplicación está corriendo en: http://localhost:${port}`);
+  logger.log(`Swagger: http://localhost:${port}/api`);
 }
 bootstrap();
